ca42f7d9c44325b77ce15f885b2990f1ed4dd206
Antonio <antonio0526@hotmail.com>
2018-06-28 17:27:37 -0700

    Added test cases for leveldb.go file

---
+++ storage/leveldb_test.go
@@ -0,0 +1,73 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//put key value pairs into database and read later
+func TestLevelDB_PutAndGet(t *testing.T){
+	//use default path
+	ldb, err := NewDatabase("")
+	assert.Nil(t, err)
+
+	err = ldb.Put([]byte("a"),[]byte("1"))
+	assert.Nil(t, err)
+	err = ldb.Put([]byte("b"),[]byte("2"))
+	assert.Nil(t, err)
+	err = ldb.Put([]byte("c"),[]byte("3"))
+	assert.Nil(t, err)
+
+	val, err := ldb.Get([]byte("a"))
+	assert.Nil(t, err)
+	assert.Equal(t,val,[]byte("1"))
+
+	val, err = ldb.Get([]byte("b"))
+	assert.Nil(t, err)
+	assert.Equal(t,val,[]byte("2"))
+
+	val, err = ldb.Get([]byte("c"))
+	assert.Nil(t, err)
+	assert.Equal(t,val,[]byte("3"))
+
+	//modify value and compare
+	err = ldb.Put([]byte("c"),[]byte("5"))
+	assert.Nil(t, err)
+	val, err = ldb.Get([]byte("c"))
+	assert.Nil(t, err)
+	assert.Equal(t,val,[]byte("5"))
+
+
+	err = ldb.Close()
+	assert.Nil(t,err)
+}
+
+//Test if database access after closing will result in error
+func TestLevelDB_AcessAfterClose(t *testing.T) {
+	//create new database
+	ldb, err := NewDatabase("../bin/test.DB")
+	assert.Nil(t, err)
+
+	//put new values in
+	err = ldb.Put([]byte("a"),[]byte("1"))
+	assert.Nil(t, err)
+
+	//Currently we should be able to read it
+	val, err := ldb.Get([]byte("a"))
+	assert.Nil(t, err)
+	assert.Equal(t,val,[]byte("1"))
+
+	//Close the database
+	err = ldb.Close()
+	assert.Nil(t,err)
+
+	//reading should return error
+	val, err = ldb.Get([]byte("a"))
+	assert.NotNil(t, err)
+	assert.Nil(t,val)
+
+	//Writing should return error
+	err = ldb.Put([]byte("b"),[]byte("2"))
+	assert.NotNil(t, err)
+}
