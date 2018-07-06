1d04a29395b5a0dd697049dc91f8f4f789b1f3f3
Antonio <antonio0526@hotmail.com>
2018-07-06 13:52:32 -0700

    modify variable name "mineAward" to "mineReward";Delete UpdateTxPool test case in miner.go since the transaction pool is not defined in miner.go anymore

--- consensus/miner_test.go
+++ consensus/miner_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 var sendAmount = int(5)
-var mineAward = int(10)
+var mineReward = int(10)
 var tip = int64(5)
 
 //mine one transaction
@@ -42,7 +42,7 @@ func TestMiner_SingleValidTx(t *testing.T) {
 	defer bc.DB.Close()
 
 	//check balance
-	checkBalance(t, addr1, addr2, bc, mineAward, 0)
+	checkBalance(t, addr1, addr2, bc, mineReward, 0)
 
 	//create 2 transactions and start mining
 	tx, err := core.NewUTXOTransaction(addr1, addr2, sendAmount, wallet, bc, tip)
@@ -53,7 +53,7 @@ func TestMiner_SingleValidTx(t *testing.T) {
 	miner := NewMiner(bc, addr1)
 	miner.Start()
 
-	checkBalance(t, addr1, addr2, bc, mineAward*2-sendAmount, sendAmount)
+	checkBalance(t, addr1, addr2, bc, mineReward*2-sendAmount, sendAmount)
 
 	teardown()
 }
@@ -82,14 +82,14 @@ func TestMiner_MineEmptyBlock(t *testing.T) {
 	defer bc.DB.Close()
 
 	//check balance
-	checkBalance(t, addr1, addr2, bc, mineAward, 0)
+	checkBalance(t, addr1, addr2, bc, mineReward, 0)
 
 	//create 2 transactions and start mining
 
 	miner := NewMiner(bc, addr1)
 	miner.Start()
 
-	checkBalance(t, addr1, addr2, bc, mineAward*2, 0)
+	checkBalance(t, addr1, addr2, bc, mineReward*2, 0)
 
 	teardown()
 }
@@ -120,7 +120,7 @@ func TestMiner_MultipleValidTx(t *testing.T) {
 	defer bc.DB.Close()
 
 	//check balance
-	checkBalance(t, addr1, addr2, bc, mineAward, 0)
+	checkBalance(t, addr1, addr2, bc, mineReward, 0)
 
 	//create 2 transactions and start mining
 	tx, err := core.NewUTXOTransaction(addr1, addr2, sendAmount, wallet, bc, tip)
@@ -132,60 +132,10 @@ func TestMiner_MultipleValidTx(t *testing.T) {
 	miner := NewMiner(bc, addr1)
 	miner.Start()
 
-	checkBalance(t, addr1, addr2, bc, mineAward*3-sendAmount*2, sendAmount*2)
+	checkBalance(t, addr1, addr2, bc, mineReward*3-sendAmount*2, sendAmount*2)
 
 	teardown()
 
-}
-
-//update tx pool
-func TestMiner_UpdateTxPool(t *testing.T) {
-
-	setup()
-
-	//create new wallet
-	wallets, _ := client.NewWallets()
-	assert.NotNil(t, wallets)
-
-	addr1 := wallets.CreateWallet()
-	assert.NotNil(t, addr1)
-
-	addr2 := wallets.CreateWallet()
-	assert.NotNil(t, addr2)
-
-	wallet := wallets.GetWallet(addr1)
-
-	//create a blockchain
-	assert.Equal(t, true, core.ValidateAddress(addr1))
-	bc, err := core.CreateBlockchain(addr1)
-	assert.Nil(t, err)
-	assert.NotNil(t, bc)
-
-	defer bc.DB.Close()
-
-	//check balance
-	checkBalance(t, addr1, addr2, bc, mineAward, 0)
-
-	//create 2 transactions and start mining
-	tx, err := core.NewUTXOTransaction(addr1, addr2, sendAmount, wallet, bc, tip)
-	assert.Nil(t, err)
-	//duplicated transactions. The second transaction will be ignored
-	core.TransactionPoolSingleton.Push(tx)
-
-	miner := NewMiner(bc, addr1)
-	miner.Start()
-
-	checkBalance(t, addr1, addr2, bc, mineAward*3-sendAmount*2, sendAmount*2)
-
-	tx1, err := core.NewUTXOTransaction(addr1, addr2, sendAmount, wallet, bc, tip)
-	core.TransactionPoolSingleton.Push(tx1)
-	core.TransactionPoolSingleton.Push(tx1)
-	UpdateTxPool(core.TransactionPoolSingleton)
-	miner.Start()
-
-	checkBalance(t, addr1, addr2, bc, mineAward*5-sendAmount*4, sendAmount*4)
-
-	teardown()
 }
 
 //TODO: test mining with invalid transactions
