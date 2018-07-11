1359c6765914a9a37e6652f9d68faf536533cd86
Alex Song <songhongli@gmail.com>
2018-07-11 16:52:36 -0700

    Add Start and Stop to Transaction Pool

--- core/transaction_pool.go
+++ core/transaction_pool.go
@@ -3,12 +3,13 @@ package core
 import (
 	"sync"
 	"container/heap"
+	"fmt"
 )
 
 // An TransactionPool is a max-heap of Transactions.
 type TransactionPool struct {
 	messageCh    chan string
-	exitCh       chan int
+	exitCh       chan bool
 	size         int
 	transactions []Transaction
 }
@@ -67,3 +68,31 @@ func (pool *TransactionPool) GetSortedTransactions() []*Transaction {
 	}
 	return sortedTransactions
 }
+
+func (pool *TransactionPool) Start(){
+	go pool.messageLoop()
+}
+
+func (pool *TransactionPool) Stop(){
+	pool.exitCh <- true
+}
+
+//todo: will change the input from string to transaction
+func (pool *TransactionPool) PushTransaction(msg string){
+//func (pool *TransactionPool) PushTransaction(tx *Transaction){
+//	pool.Push(tx)
+	fmt.Println(msg)
+}
+
+func (pool *TransactionPool) messageLoop() {
+	for {
+		select {
+		case <-pool.exitCh:
+			fmt.Println("Quit Transaction Pool")
+			return
+		case msg := <-pool.messageCh:
+			pool.PushTransaction(msg)
+		}
+	}
+}
+
