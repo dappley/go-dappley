06a3a726099339bbcf76125b915526c327b1d876
chaoswlz <wulize1994@gmail.com>
2018-07-26 14:41:16 -0700

    change testcase for transaction pool

--- core/transaction_pool_test.go
+++ core/transaction_pool_test.go
@@ -1,52 +1,72 @@
 package core
 
 import (
+	"container/heap"
 	"testing"
 
 	"github.com/dappley/go-dappley/util"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTranstionHeapOperations(t *testing.T) {
-	t1 := Transaction{
-		ID:   util.GenerateRandomAoB(1),
-		Vin:  generateFakeTxInputs(),
-		Vout: generateFakeTxOutputs(),
-		Tip:  5,
-	}
-	t2 := Transaction{
-		ID:   util.GenerateRandomAoB(1),
-		Vin:  generateFakeTxInputs(),
-		Vout: generateFakeTxOutputs(),
-		Tip:  10,
-	}
-	t3 := Transaction{
-		ID:   util.GenerateRandomAoB(1),
-		Vin:  generateFakeTxInputs(),
-		Vout: generateFakeTxOutputs(),
-		Tip:  2,
-	}
-	t4 := Transaction{
-		ID:   util.GenerateRandomAoB(1),
-		Vin:  generateFakeTxInputs(),
-		Vout: generateFakeTxOutputs(),
-		Tip:  20,
-	}
+var t1 = Transaction{
+	ID:   util.GenerateRandomAoB(1),
+	Vin:  generateFakeTxInputs(),
+	Vout: generateFakeTxOutputs(),
+	Tip:  2,
+}
+var t2 = Transaction{
+	ID:   util.GenerateRandomAoB(1),
+	Vin:  generateFakeTxInputs(),
+	Vout: generateFakeTxOutputs(),
+	Tip:  5,
+}
+var t3 = Transaction{
+	ID:   util.GenerateRandomAoB(1),
+	Vin:  generateFakeTxInputs(),
+	Vout: generateFakeTxOutputs(),
+	Tip:  10,
+}
+var t4 = Transaction{
+	ID:   util.GenerateRandomAoB(1),
+	Vin:  generateFakeTxInputs(),
+	Vout: generateFakeTxOutputs(),
+	Tip:  20,
+}
+
+var expectPopOrder = []int64{20, 10, 5, 2}
+
+var popInputOrder = []struct {
+	order []Transaction
+}{
+	{[]Transaction{t4, t3, t2, t1}},
+	{[]Transaction{t1, t2, t3, t4}},
+	{[]Transaction{t2, t1, t4, t3}},
+	{[]Transaction{t4, t1, t3, t2}},
+}
+
+//transaction pool push function
+func TestTxPoolPush(t *testing.T) {
 	txPool := GetTxnPoolInstance()
-
-	txPool.Push(t1)
-
+	heap.Push(txPool, t1)
 	assert.Equal(t, 1, txPool.Len())
-
-	txPool.Push(t2)
+	heap.Push(txPool, t2)
 	assert.Equal(t, 2, txPool.Len())
-	txPool.Push(t3)
-	txPool.Push(t4)
+	heap.Push(txPool, t3)
+	heap.Push(txPool, t4)
 	assert.Equal(t, 4, txPool.Len())
+}
 
-	for txPool.Len() > 0 {
-		txPool.Pop()
+func TestTranstionPoolPop(t *testing.T) {
+	for _, tt := range popInputOrder {
+		var popOrder = []int64{}
+		txPool := GetTxnPoolInstance()
+		for _, t := range tt.order {
+			heap.Push(txPool, t)
+		}
+		for txPool.Len() > 0 {
+			popOrder = append(popOrder, heap.Pop(txPool).(Transaction).Tip)
+		}
+		assert.Equal(t, expectPopOrder, popOrder)
 	}
-	assert.Equal(t, 0, txPool.Len())
 
 }
