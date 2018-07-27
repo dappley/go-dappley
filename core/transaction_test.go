8750cbc6bcf5bd66b58866d39352a574625b62ab
chaoswlz <wulize1994@gmail.com>
2018-07-27 15:11:44 -0700

    write test case for trimmed copy

--- core/transaction_test.go
+++ core/transaction_test.go
@@ -27,6 +27,39 @@ func GenerateFakeTxOutputs() []TXOutput {
 	}
 }
 
+func TestTrimmedCopy(t *testing.T) {
+	var t1 = Transaction{
+		ID:   util.GenerateRandomAoB(1),
+		Vin:  GenerateFakeTxInputs(),
+		Vout: GenerateFakeTxOutputs(),
+		Tip:  2,
+	}
+
+	t2 := t1.TrimmedCopy()
+
+	t3 := NewCoinbaseTX("13ZRUc4Ho3oK3Cw56PhE5rmaum9VBeAn5F", "")
+	t4 := t3.TrimmedCopy()
+	assert.Equal(t, t1.ID, t2.ID)
+	assert.Equal(t, t1.Tip, t2.Tip)
+	assert.Equal(t, t1.Vout, t2.Vout)
+	for index, vin := range t2.Vin {
+		assert.Nil(t, vin.Signature)
+		assert.Nil(t, vin.PubKey)
+		assert.Equal(t, t1.Vin[index].Txid, vin.Txid)
+		assert.Equal(t, t1.Vin[index].Vout, vin.Vout)
+	}
+
+	assert.Equal(t, t3.ID, t4.ID)
+	assert.Equal(t, t3.Tip, t4.Tip)
+	assert.Equal(t, t3.Vout, t4.Vout)
+	for index, vin := range t4.Vin {
+		assert.Nil(t, vin.Signature)
+		assert.Nil(t, vin.PubKey)
+		assert.Equal(t, t3.Vin[index].Txid, vin.Txid)
+		assert.Equal(t, t3.Vin[index].Vout, vin.Vout)
+	}
+}
+
 //test IsCoinBase and NewCoinbaseTX function
 func TestIsCoinBase(t *testing.T) {
 	var t1 = Transaction{
