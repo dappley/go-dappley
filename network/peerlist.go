42000de9baaadf8747310d9bd7c96532c742185a
chaoswlz <wulize1994@gmail.com>
2018-07-31 17:48:18 -0700

    add limit for peer list

--- network/peerlist.go
+++ network/peerlist.go
@@ -1,35 +1,35 @@
 package network
 
 import (
-	"github.com/multiformats/go-multiaddr"
-	"github.com/gogo/protobuf/proto"
-	"github.com/dappley/go-dappley/network/pb"
-	"github.com/libp2p/go-libp2p-peer"
 	"fmt"
-	logger "github.com/sirupsen/logrus"
 
+	"github.com/dappley/go-dappley/network/pb"
+	"github.com/gogo/protobuf/proto"
+	"github.com/libp2p/go-libp2p-peer"
+	"github.com/multiformats/go-multiaddr"
+	logger "github.com/sirupsen/logrus"
 )
 
-type PeerList struct{
-	peers	[]*Peer
+type PeerList struct {
+	peers [64]*Peer
 }
 
-type Peer struct{
-	peerid 		peer.ID
-	addr   		multiaddr.Multiaddr
+type Peer struct {
+	peerid peer.ID
+	addr   multiaddr.Multiaddr
 }
 
-func CreatePeerFromMultiaddr(targetFullAddr multiaddr.Multiaddr) (*Peer,error){
+func CreatePeerFromMultiaddr(targetFullAddr multiaddr.Multiaddr) (*Peer, error) {
 	//get pid
 	pid, err := targetFullAddr.ValueForProtocol(multiaddr.P_IPFS)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	//get peer id
 	peerid, err := peer.IDB58Decode(pid)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	// Decapsulate the /ipfs/<peerID> part from the targetFullAddr
@@ -39,12 +39,12 @@ func CreatePeerFromMultiaddr(targetFullAddr multiaddr.Multiaddr) (*Peer,error){
 	return &Peer{
 		peerid,
 		targetAddr,
-	},nil
+	}, nil
 }
 
-func CreatePeerFromString(targetFullAddr string) (*Peer,error){
-	ma,err := multiaddr.NewMultiaddr(targetFullAddr)
-	if err!=nil {
+func CreatePeerFromString(targetFullAddr string) (*Peer, error) {
+	ma, err := multiaddr.NewMultiaddr(targetFullAddr)
+	if err != nil {
 		return nil, err
 	}
 	return CreatePeerFromMultiaddr(ma)
@@ -63,9 +63,9 @@ func NewPeerList(p []*Peer) *PeerList {
 //create new peerList with strings
 func NewPeerListStr(strs []string) *PeerList {
 	var ps []*Peer
-	for _, str := range strs{
+	for _, str := range strs {
 		peer, err := CreatePeerFromString(str)
-		if err!= nil {
+		if err != nil {
 			logger.Warn("Address Unrecognized:", str)
 		}
 		ps = append(ps, peer)
@@ -74,16 +74,16 @@ func NewPeerListStr(strs []string) *PeerList {
 }
 
 //Add a multiadress.
-func (pl *PeerList) Add(p *Peer){
+func (pl *PeerList) Add(p *Peer) {
 	//add only if it is not already existed in the list
-	if !pl.IsInPeerlist(p){
+	if !pl.IsInPeerlist(p) {
 		pl.peers = append(pl.peers, p)
 	}
 }
 
 //add multiple addresses
-func (pl *PeerList) AddMultiple(ps []*Peer){
-	for _,p := range ps{
+func (pl *PeerList) AddMultiple(ps []*Peer) {
+	for _, p := range ps {
 		pl.Add(p)
 	}
 }
@@ -95,9 +95,9 @@ func (pl *PeerList) MergePeerlist(newpl *PeerList) {
 
 //find the peers in newpl that are not contained in current pl
 func (pl *PeerList) FindNewPeers(newpl *PeerList) *PeerList {
-	retpl:=&PeerList{}
-	for _, m := range newpl.GetPeerlist(){
-		if !pl.IsInPeerlist(m){
+	retpl := &PeerList{}
+	for _, m := range newpl.GetPeerlist() {
+		if !pl.IsInPeerlist(m) {
 			retpl.Add(m)
 		}
 	}
@@ -110,8 +110,8 @@ func (pl *PeerList) GetPeerlist() []*Peer {
 }
 
 //Check if a multiaddress is already existed in the list
-func (pl *PeerList) IsInPeerlist(p *Peer) bool{
-	for _,ps := range pl.peers{
+func (pl *PeerList) IsInPeerlist(p *Peer) bool {
+	for _, ps := range pl.peers {
 		if ps.peerid.String() == p.peerid.String() {
 			return true
 		}
@@ -120,32 +120,32 @@ func (pl *PeerList) IsInPeerlist(p *Peer) bool{
 }
 
 //convert to protobuf
-func (p *Peer) ToProto() proto.Message{
+func (p *Peer) ToProto() proto.Message {
 	return &networkpb.Peer{
-		Peerid:		peer.IDB58Encode(p.peerid),
-		Addr: 		p.addr.String(),
+		Peerid: peer.IDB58Encode(p.peerid),
+		Addr:   p.addr.String(),
 	}
 }
 
 //convert from protobuf
-func (p *Peer) FromProto(pb proto.Message) error{
+func (p *Peer) FromProto(pb proto.Message) error {
 	pid, err := peer.IDB58Decode(pb.(*networkpb.Peer).Peerid)
-	if err!=nil {
+	if err != nil {
 		return err
 	}
 	p.peerid = pid
 	p.addr, err = multiaddr.NewMultiaddr(pb.(*networkpb.Peer).Addr)
-	if err!=nil {
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
 //convert to protobuf
-func (pl *PeerList) ToProto() proto.Message{
+func (pl *PeerList) ToProto() proto.Message {
 
 	var peerlist []*networkpb.Peer
-	for i := range pl.peers{
+	for i := range pl.peers {
 		peerlist = append(peerlist, pl.peers[i].ToProto().(*networkpb.Peer))
 	}
 
@@ -158,7 +158,7 @@ func (pl *PeerList) ToProto() proto.Message{
 func (pl *PeerList) FromProto(pb proto.Message) {
 	peerlist := pb.(*networkpb.Peerlist).Peerlist
 	pl.peers = nil
-	for _, peer := range peerlist{
+	for _, peer := range peerlist {
 		p := &Peer{}
 		p.FromProto(peer)
 		pl.peers = append(pl.peers, p)
