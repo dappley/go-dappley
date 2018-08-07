e8e254d2f52390da097ec6aa2f65e1cd2387ad7b
Antonio <antonio0526@hotmail.com>
2018-08-07 16:12:00 -0700

    Add test cases to decodeMessage function

--- network/stream_test.go
+++ network/stream_test.go
@@ -6,27 +6,45 @@ import (
 )
 
 func TestStream_decode(t *testing.T){
-	//mock an integer array
-	data := []byte{1,2,3,4}
-	//encode data
-	encodeData := encodeMessage(data)
-	//decode data
-	decodeData, err:= decodeMessage(encodeData)
-	//there should be no error
-	assert.Nil(t,err)
-	//the data should be the same as before
-	assert.ElementsMatch(t, data, decodeData)
+	tests := []struct{
+		name 		string
+		input 		[]byte
+		retData 	[]byte
+		retErr 		error
+	}{
+		{
+			name: 		"CorrectData",
+			input: 		[]byte{0x7E,0x7E,0x55,0x44,0x7F,0x7F,0x00},
+			retData:   	[]byte{0x55,0x44},
+			retErr: 	nil,
+		},
+		{
+			name: 		"IncorrectStartingByte",
+			input: 		[]byte{0x7E,0x55,0x44,0x7F,0x7F,0x00},
+			retData:   	nil,
+			retErr: 	ErrInvalidMessageFormat,
+		},
+		{
+			name: 		"IncorrectEndingingByte",
+			input: 		[]byte{0x7E,0x7E,0x55,0x44,0x7F,0x7F,0x01},
+			retData:   	nil,
+			retErr: 	ErrInvalidMessageFormat,
+		},
+		{
+			name: 		"IncorrectData",
+			input: 		[]byte{0x55,0x44},
+			retData:   	nil,
+			retErr: 	ErrInvalidMessageFormat,
+		},
+	}
 
-}
-
-func TestStream_decodeFragmentedMsg(t *testing.T){
-	//mock an integer array that does not follow the format of encoded data
-	data := []byte{1,2,3,4}
-	//decode
-	decodeData, err:= decodeMessage(data)
-	//there should be an error returned
-	assert.Equal(t,ErrInvalidMessageFormat,err)
-	assert.Nil(t, decodeData)
+	for _,tt := range tests{
+		t.Run(tt.name,func(t *testing.T){
+			ret,err := decodeMessage(tt.input)
+			assert.Equal(t,tt.retData,ret)
+			assert.Equal(t,tt.retErr,err)
+		})
+	}
 }
 
 func TestStream_containStartingBytes(t *testing.T){
