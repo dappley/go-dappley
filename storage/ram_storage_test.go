d7bfec102dfa2920edb06f2c5b120b4781dad916
chaoswlz <wulize1994@gmail.com>
2018-08-07 17:03:23 -0700

    unit tests for storageRam.go

--- storage/ram_storage_test.go
+++ storage/ram_storage_test.go
@@ -1,32 +1,40 @@
 package storage
 
 import (
+	"errors"
+	"sync"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNewRamStorage(t *testing.T) {
+	r := NewRamStorage()
+	assert.Equal(t, r.data, new(sync.Map))
+}
 
 //test get and put methods
 func TestRamStorage_GetandPut(t *testing.T) {
 	r := NewRamStorage()
 
 	//setup various key scenario
-	var keyValuePairs = []struct{
-		key 	[]byte
-		value 	[]byte
-	} {
-		{[]byte("a"),[]byte("b")},	//lowercase letter
-		{[]byte("A"),[]byte("B")},	//CAPITAL letter
-		{[]byte("1"),[]byte("2")},	//number
-		{[]byte(""),[]byte("3")},	//empty string
+	var keyValuePairs = []struct {
+		key   []byte
+		value []byte
+	}{
+		{[]byte("a"), []byte("b")}, //lowercase letter
+		{[]byte("A"), []byte("B")}, //CAPITAL letter
+		{[]byte("1"), []byte("2")}, //number
+		{[]byte(""), []byte("3")},  //empty string
 	}
 
 	//for each key value pair. Store it, read it and compare if the value stays unchanged
-	for i,kv := range keyValuePairs {
-		t.Run(string(i),func(t *testing.T){
-			r.Put(kv.key,kv.value)
-			v,err := r.Get(kv.key)
+	for i, kv := range keyValuePairs {
+		t.Run(string(i), func(t *testing.T) {
+			r.Put(kv.key, kv.value)
+			v, err := r.Get(kv.key)
 			assert.Nil(t, err)
-			assert.Equal(t, v,kv.value)
+			assert.Equal(t, v, kv.value)
 		})
 	}
 
@@ -34,7 +42,7 @@ func TestRamStorage_GetandPut(t *testing.T) {
 	_, err := r.Get([]byte("d"))
 
 	//there should be an error returned
-	assert.NotNil(t,err)
+	assert.Equal(t, err, errors.New("Key is invalid"))
 }
 
 //test close method
@@ -42,7 +50,7 @@ func TestRamStorage_Close(t *testing.T) {
 	r := NewRamStorage()
 
 	//write a key-value pair
-	r.Put([]byte("1"),[]byte("2"))
+	r.Put([]byte("1"), []byte("2"))
 
 	//close the storage
 	r.Close()
@@ -51,7 +59,7 @@ func TestRamStorage_Close(t *testing.T) {
 	v, err := r.Get([]byte("1"))
 
 	//there should be an error returned
-	assert.NotNil(t,err)
+	assert.Equal(t, err, errors.New("Key is invalid"))
 	//there should be not value returned
 	assert.Nil(t, v)
 }
@@ -62,16 +70,16 @@ func TestRamStorage_IndependantStorage(t *testing.T) {
 	r2 := NewRamStorage()
 
 	//write a key-value pair in r1 storage should not affect r2
-	r1.Put([]byte("1"),[]byte("a"))
-	r2.Put([]byte("1"),[]byte("b"))
+	r1.Put([]byte("1"), []byte("a"))
+	r2.Put([]byte("1"), []byte("b"))
 
 	//get value in storage r1 and compare with original value
-	v1, err:= r1.Get([]byte("1"))
+	v1, err := r1.Get([]byte("1"))
 	assert.Nil(t, err)
-	assert.Equal(t,[]byte("a"),v1 )
+	assert.Equal(t, []byte("a"), v1)
 
 	//get value in storage r2
-	v2, err:= r2.Get([]byte("1"))
+	v2, err := r2.Get([]byte("1"))
 	assert.Nil(t, err)
-	assert.Equal(t,[]byte("b"),v2 )
+	assert.Equal(t, []byte("b"), v2)
 }
