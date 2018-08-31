6befce329bd8cfbbb8ee238ca2cd7315881c3ef2
ggcodemasta <grant_yao@live.ca>
2018-08-30 17:25:32 -0700

    added tree structure

--- common/tree.go
+++ common/tree.go
@@ -1,1 +1,109 @@
 package common
+
+import (
+	"errors"
+	logger "github.com/sirupsen/logrus"
+)
+
+type Entry struct{
+	key interface{}
+	value interface{}
+}
+
+
+var (
+	ErrNodeNotFound = errors.New("ERROR: Node not found in tree")
+	ErrCantCreateEmptyNode = errors.New("ERROR: Node index and value must not be empty")
+
+)
+
+//entries include the node's entry itself as the first entry and its childrens' entry following
+type node struct {
+	Entries []Entry
+	Parent *node
+	Children []*node
+	Height uint
+}
+
+type Tree struct {
+	Root *node
+	MaxHeight uint
+	Found *node
+	Searching bool
+}
+type Test struct {
+	Num uint
+}
+
+
+func (n *node) hasChildren() bool{
+	if len(n.Children) > 0 {
+		return true
+	}
+	return false
+}
+
+func (t *Tree) NewNode(index interface{}, value interface{}) (*node, error){
+	if index == nil || value == nil {
+		return nil, ErrCantCreateEmptyNode
+	}
+	return &node{[]Entry{Entry{index,value,}}, nil, nil, 1}, nil
+}
+
+func NewTree(rootNodeIndex interface{}, rootNodeValue interface{}) *Tree{
+	r := node{[]Entry{Entry{rootNodeIndex,rootNodeValue,}}, nil, nil, 1}
+	return &Tree{&r, r.Height , nil, false}
+}
+
+
+func (t *Tree) RecursiveFind (parent *node, index interface{}) {
+	if !parent.hasChildren() ||  t.Searching == false {
+		logger.Debug(parent.Entries[0].key," has no children")
+		return
+	}
+	for i:=0;i< len(parent.Children);i++  {
+		if parent.Children[i].Entries[0].key == index{
+			logger.Debug("found! ", index, " under ", parent.Entries[0].key)
+			t.Searching = false
+			t.Found = parent.Children[i]
+		}else{
+			if t.Searching {
+				t.RecursiveFind(parent.Children[i], index)
+			}
+		}
+	}
+}
+
+//Search from root, use if you have no closer known nodes upstream
+func (t *Tree) Get(parent *node, index interface{}){
+	t.Searching = true
+	if t.Root.Entries[0].key == index{
+		logger.Debug("found! ", index, ", is root")
+		t.Found = t.Root
+		return
+	}
+	t.RecursiveFind(parent, index)
+}
+
+func (t *Tree) SearchParentNodeAndAddChild( startNode *node, parentIndex interface{} , childIndex interface{}, childValue interface{}){
+	child,_ := t.NewNode(childIndex, childValue)
+	t.Get(t.Root, parentIndex)
+	parent := t.Found
+	parent.AddChild(child)
+}
+
+func (parent *node) AddChild(child *node){
+	parent.Children = append(parent.Children, child)
+	parent.Entries = append(parent.Entries, child.Entries[0])
+	child.Parent = parent
+}
+
+//attach a tree's root node to a specific node of another tree through node index
+func (t *Tree) appendTree(tree *Tree, mergeIndex interface{}) {
+	t.Get(t.Root, mergeIndex)
+	t.Found.AddChild(tree.Root)
+	//is higher than original tree after appending
+	if tree.MaxHeight + t.Found.Height > t.MaxHeight{
+		t.MaxHeight = tree.MaxHeight + t.Found.Height
+	}
+}
--- common/tree_test.go
+++ common/tree_test.go
@@ -1,1 +1,86 @@
 package common
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"strings"
+)
+
+func setupIntTree() *Tree{
+	tree:= NewTree(0,0)
+	parent := tree.Root
+	//add 100000 nodes unto the tree
+	for i:=1;i<100000;i++  {
+		newNode := node{[]Entry{Entry{i,i}},parent,nil, parent.Height+1}
+		parent.Children = append(parent.Children, &newNode)
+		//if is true, create a new branch, else build existing branch
+		if(getBool()){
+			parent = &newNode
+			tree.MaxHeight++
+		}
+	}
+	return tree
+}
+func setupAlphabetTree() *Tree{
+	alphabets:= "abcdefghijklmnopqrstuvwxyz"
+	alphabetSlice := strings.Split(alphabets, "")
+	tree:= NewTree("a","a")
+	parent := tree.Root
+	//add 26 nodes unto the tree
+	for i:=1;i< len(alphabetSlice);i++  {
+		newNode := node{[]Entry{Entry{alphabetSlice[i],alphabetSlice[i]}},parent,nil, parent.Height+1}
+		parent.Children = append(parent.Children, &newNode)
+		//if is true, create a new branch, else build existing branch
+		if(getBool()){
+			parent = &newNode
+			tree.MaxHeight++
+		}
+	}
+	return tree
+}
+func Test_RecursiveFind(t *testing.T){
+	tree := setupIntTree()
+	//run find lots of times
+	for i:=50000; i<50500;i++ {
+		tree.Get(tree.Root, i)
+		assert.Equal(t, i, tree.Found.Entries[0].value)
+	}
+}
+
+func Test_SearchParentNodeAndAddChild(t *testing.T){
+	tree := setupIntTree()
+	//add child {asd:asd} to 90000 block
+	tree.SearchParentNodeAndAddChild(
+		tree.Root,90000, "asd", "asd")
+
+	tree.Get(tree.Root, "asd")
+	assert.Equal(t, "asd", tree.Found.Entries[0].value)
+	assert.Equal(t, 90000, tree.Found.Parent.Entries[0].value)
+
+}
+
+func Test_appendTree(t *testing.T){
+
+	mergeIndex := 99980
+	mergeHeight := mergeIndex+1 //0 based index offset
+
+	t1 := setupIntTree()
+	t2 := setupAlphabetTree()
+	t1.appendTree(t2, mergeIndex)
+
+	t1.Get(t1.Root, "b")
+	assert.Equal(t, "b", t1.Found.Entries[0].value )
+	t1.Get(t1.Root, "y")
+	assert.Equal(t, "y", t1.Found.Entries[0].value )
+
+
+	assert.Equal(t, uint(mergeHeight)+t2.MaxHeight , t1.MaxHeight)
+}
+
+func getBool() bool {
+	//rand.Seed(time.Now().UnixNano())
+	//return rand.Intn(10) >= 5
+	return true
+}
+
+
