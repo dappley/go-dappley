9a87fc66fec26a8528464db1d95aff14615f5b0d
chaoswlz <wulize1994@gmail.com>
2018-10-03 11:07:21 -0700

    change hash print out byte -> string

--- network/node.go
+++ network/node.go
@@ -292,7 +292,7 @@ func (n *Node) prepareData(msgData proto.Message, cmd string, uniOrBroadcast int
 }
 
 func (n *Node) BroadcastBlock(block *core.Block) error {
-	logger.Debug("Node: BroadcastBlock: Hash:", block.GetHash(), ", Height:", block.GetHeight())
+	logger.Debug("Node: BroadcastBlock: Hash:", hex.EncodeToString(block.GetHash()), ", Height:", block.GetHeight())
 	data, err := n.prepareData(block.ToProto(), SyncBlock, Broadcast)
 	if err != nil {
 		return err
