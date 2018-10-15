b4a40d029cb972228758d61b6bcd8f656dabe49b
Liwanghong <wanghong@dappworks.cn>
2018-10-15 10:42:46 +0800

    Change testcase sync waiting time

--- consensus/dpos_integration_test.go
+++ consensus/dpos_integration_test.go
@@ -110,7 +110,7 @@ func TestDpos_MultipleMiners(t *testing.T) {
 	}
 	endTime := time.Now().Unix()
 	//Waiting block sync to other nodes
-	time.Sleep(2)
+	time.Sleep(time.Second * 2)
 
 	for i := 0; i < len(miners); i++ {
 		v := dposArray[i]
