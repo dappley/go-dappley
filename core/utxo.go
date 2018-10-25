ba0c0b455e62143029ef3650c8a7e7969990ea4d
ggcodemasta <grant_yao@live.ca>
2018-10-25 14:25:14 -0700

    rename utxo function

--- core/utxo.go
+++ core/utxo.go
@@ -149,7 +149,7 @@ func (utxos UTXOIndex) FindUTXOByVin(pubkeyHash []byte, txid []byte, vout int) *
 
 // Update removes the UTXOs spent in the transactions in newBlk from the index and adds UTXOs generated in the
 // transactions to the index. The index will be saved to db as a result. If saving failed, index won't be updated.
-func (utxos *UTXOIndex) UpdateUtxoStateAndCheckForDoubleSpending(newBlk *Block, db storage.Storage) error {
+func (utxos *UTXOIndex) TryUpdateUtxoState(newBlk *Block, db storage.Storage) error {
 	// Create a copy of the index so operations below are only temporal
 	tempIndex := utxos.deepCopy()
 
--- core/utxo_test.go
+++ core/utxo_test.go
@@ -175,7 +175,7 @@ func TestUpdate(t *testing.T) {
 
 	blk := GenerateUtxoMockBlockWithoutInputs()
 	utxoIndex := NewUTXOIndex()
-	utxoIndex.UpdateUtxoStateAndCheckForDoubleSpending(blk, db)
+	utxoIndex.TryUpdateUtxoState(blk, db)
 	utxoIndexInDB := LoadUTXOIndex(db)
 
 	// Assert that both the original instance and the database copy are updated correctly
@@ -198,7 +198,7 @@ func TestUpdate_Failed(t *testing.T) {
 
 	blk := GenerateUtxoMockBlockWithoutInputs()
 	utxoIndex := NewUTXOIndex()
-	err := utxoIndex.UpdateUtxoStateAndCheckForDoubleSpending(blk, db)
+	err := utxoIndex.TryUpdateUtxoState(blk, db)
 	assert.Equal(t, simulatedFailure, err)
 	assert.Equal(t, 0, len(utxoIndex.index[string(address1Hash.GetPubKeyHash())]))
 }
