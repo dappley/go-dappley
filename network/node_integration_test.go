7cf42c46443b2856efb12607438cc48af9efccdc
ggcodemasta <grant_yao@live.ca>
2018-10-25 18:40:59 -0700

    changed node integ tests that were being mocked to actual data and removed some obvious comments

--- network/node_integration_test.go
+++ network/node_integration_test.go
@@ -21,12 +21,10 @@
 package network
 
 import (
+	"encoding/hex"
 	"github.com/dappley/go-dappley/core"
-
-	"github.com/dappley/go-dappley/mocks"
 	"github.com/dappley/go-dappley/storage"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"testing"
 	"time"
 )
@@ -70,53 +68,36 @@ func TestNetwork_Setup(t *testing.T) {
 }
 
 
-func TestNetwork_SendBlock(t *testing.T){
-	bp0 := new(mocks.BlockPoolInterface)
-	bp1 := new(mocks.BlockPoolInterface)
-
+func TestNetwork_BroadcastBlock(t *testing.T){
 	//setup node 0
 	db0 := storage.NewRamStorage()
 	defer db0.Close()
-
-
-	bp0.On("SetBlockchain", mock.Anything).Return(nil)
-	bp0.On("BlockRequestCh").Return(make(chan core.BlockRequestPars))
 	bc0 := core.CreateBlockchain(core.Address{"17DgRtQVvaytkiKAfXx9XbV23MESASSwUz"}, db0, nil,128)
-	bp1.On("SetBlockchain", mock.Anything)
-	bp1.On("BlockRequestCh").Return(make(chan core.BlockRequestPars))
-	bp1.On("Push", mock.Anything,mock.Anything).Return(nil)
-	bc0.SetBlockPool(bp0)
-	assert.Equal(t, bc0.GetBlockPool(),  bp0)
-	bp0.AssertCalled(t, "SetBlockchain", mock.Anything)
 	n0 := FakeNodeWithPidAndAddr(bc0,"QmWyMUMBeWxwU4R5ukBiKmSiGT8cDqmkfrXCb2qTVHpofJ","/ip4/192.168.10.110/tcp/10000")
 
 	//setup node 1
 	db1 := storage.NewRamStorage()
 	defer db1.Close()
 
-
 	bc1 := core.CreateBlockchain(core.Address{"17DgRtQVvaytkiKAfXx9XbV23MESASSwUz"}, db1, nil,128)
-	bc1.SetBlockPool(bp1)
-
-	bp0.AssertCalled(t, "SetBlockchain", mock.Anything)
-
 	n1 := FakeNodeWithPidAndAddr(bc1,"QmWyMUMBeWxwU4R5ukBiKmSiGT8cDqmkfrXCb2qTVHpofJ","/ip4/192.168.10.110/tcp/10001")
 
 	n0.Start(test_port3)
-	time.Sleep(1*time.Second)
-	bp0.AssertCalled(t, "BlockRequestCh")
 	n1.Start(test_port4)
-	time.Sleep(1*time.Second)
-	bp1.AssertCalled(t, "BlockRequestCh")
-	//add node0 as a stream peer in node1 and node2
-	err := n1.AddStream(n0.GetPeerID(), n0.GetPeerMultiaddr())
+
+	err := n1.AddStream(n0.GetPeerID(),n0.GetPeerMultiaddr())
 	assert.Nil(t, err)
 
-	//node 0 broadcast a block
 	blk := core.GenerateMockBlock()
 	n0.BroadcastBlock(blk)
-	time.Sleep(2*time.Second)
-	bp1.AssertCalled(t, "Push", mock.Anything, mock.Anything)
+
+	//wait for node 1 to receive response
+	core.WaitDoneOrTimeout(func() bool {
+		blk, _ := n1.recentlyRcvedDapMsgs.Load(hex.EncodeToString(blk.GetHash()))
+		return blk != nil
+	}, 5)
+
+	assert.True(t, true)
 }
 
 func TestNode_SyncPeers(t *testing.T) {
@@ -163,55 +144,36 @@ func TestNode_SyncPeers(t *testing.T) {
 
 func TestNode_RequestBlockUnicast(t *testing.T) {
 
-	bp0 := new(mocks.BlockPoolInterface)
-	bp1 := new(mocks.BlockPoolInterface)
-
-	bp0.On("SetBlockchain", mock.Anything).Return(nil)
-	bp0.On("BlockRequestCh").Return(make(chan core.BlockRequestPars))
-	bp1.On("SetBlockchain", mock.Anything).Return(nil)
-	bp1.On("BlockRequestCh").Return(make(chan core.BlockRequestPars))
-	bp1.On("Push", mock.Anything,mock.Anything).Return(nil)
-
 	//setup node 0
 	db0 := storage.NewRamStorage()
 	defer db0.Close()
-
 	bc0 := core.CreateBlockchain(core.Address{"17DgRtQVvaytkiKAfXx9XbV23MESASSwUz"}, db0, nil,128)
-	bc0.SetBlockPool(bp0)
 
-	n0 := FakeNodeWithPidAndAddr(bc0, "QmWyMUMBeWxwU4R5ukBiKmSiGT8cDqmkfrXCb2qTVHpofJ", "/ip4/192.168.10.110/tcp/10000")
+	n0 := FakeNodeWithPidAndAddr(bc0,"QmWyMUMBeWxwU4R5ukBiKmSiGT8cDqmkfrXCb2qTVHpofJ","/ip4/192.168.10.110/tcp/10000")
 
 	//setup node 1
 	db1 := storage.NewRamStorage()
 	defer db1.Close()
-
 	bc1 := core.CreateBlockchain(core.Address{"17DgRtQVvaytkiKAfXx9XbV23MESASSwUz"}, db1, nil,128)
-
-	bc1.SetBlockPool(bp1)
 	n1 := FakeNodeWithPidAndAddr(bc1,"QmWyMUMBeWxwU4R5ukBiKmSiGT8cDqmkfrXCb2qTVHpofJ","/ip4/192.168.10.110/tcp/10000")
 
 	n0.Start(test_port9)
-	time.Sleep(1*time.Second)
-	bp0.AssertCalled(t, "BlockRequestCh")
-
 	n1.Start(test_port5)
-	time.Sleep(1*time.Second)
-	bp1.AssertCalled(t, "BlockRequestCh")
 
-	//add node0 as a stream peer in node1 and node2
-	err := n1.AddStream(n0.GetPeerID(), n0.GetPeerMultiaddr())
+	err := n1.AddStream(n0.GetPeerID(),n0.GetPeerMultiaddr())
 	assert.Nil(t, err)
 
-	//generate a block and store it in node0 blockchain
 	blk := core.GenerateMockBlock()
 	err = n0.bc.GetDb().Put(blk.GetHash(), blk.Serialize())
 	assert.Nil(t, err)
 
-	//node1 request the block
 	n1.RequestBlockUnicast(blk.GetHash(),n0.GetPeerID())
-
 	//wait for node 1 to receive response
-	time.Sleep(2*time.Second)
-	bp1.AssertCalled(t, "Push", mock.Anything, mock.Anything)
+	core.WaitDoneOrTimeout(func() bool {
+		blk, _ := n1.recentlyRcvedDapMsgs.Load(hex.EncodeToString(blk.GetHash()))
+		return blk != nil
+	}, 5)
+
+	assert.True(t, true)
 
 }
