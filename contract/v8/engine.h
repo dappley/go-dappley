a0249b1ae1ec5d2f56302101932fb7c4accbea81
Ricky Wong Wai Kiu <rwong@omnisolu.com>
2018-11-13 19:17:12 -0800

    Add transfer function to libdappleyv8

--- contract/v8/engine.h
+++ contract/v8/engine.h
@@ -6,17 +6,17 @@
 extern "C" {
 #endif
     typedef bool (*FuncVerifyAddress)(const char *address);
+    typedef int (*FuncTransfer)(void *handler, const char *to, const char *amount, const char *tip);
     typedef char* (*FuncStorageGet)(void *address, const char *key);
     typedef int (*FuncStorageSet)(void *address, const char *key, const char *value);
     typedef int (*FuncStorageDel)(void *address, const char *key);
 
     EXPORT void Initialize();
-    EXPORT int executeV8Script(const char *sourceCode, uintptr_t handler, char **result) ;
-    EXPORT void InitializeBlockchain(FuncVerifyAddress verifyAddress);
+    EXPORT int executeV8Script(const char *sourceCode, uintptr_t handler, char **result);
+    EXPORT void InitializeBlockchain(FuncVerifyAddress verifyAddress, FuncTransfer transfer);
     EXPORT void InitializeStorage(FuncStorageGet get, FuncStorageSet set, FuncStorageDel del);
     EXPORT void InitializeSmartContract(char* source);
     EXPORT void DisposeV8();
 #ifdef __cplusplus
 }
 #endif
-
