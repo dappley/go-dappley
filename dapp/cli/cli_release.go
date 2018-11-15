a3bf2f2bc485e472e88c75c9c06d6538ba84df67
winterweixiaodong <xiaodongwei2010@gmail.com>
2018-11-15 10:42:05 -0800

    add license comments

--- dapp/cli/cli_release.go
+++ dapp/cli/cli_release.go
@@ -583,6 +583,12 @@ func sendCommandHandler(ctx context.Context, client interface{}, flags cmdFlags)
 }
 
 func helpCommandHandler(ctx context.Context, client interface{}, flags cmdFlags) {
+	fmt.Println("-----------------------------------------------------------------")
+	fmt.Println("Command: cli ", "createWallet")
+	fmt.Printf("Usage Example: cli createWallet\n")
+	fmt.Println("-----------------------------------------------------------------")
+	fmt.Println("Command: cli ", "listAddresses")
+	fmt.Printf("Usage Example: cli listAddresses\n")
 	for cmd, pars := range cmdFlagsMap {
 		fmt.Println("-----------------------------------------------------------------")
 		fmt.Println("Command: cli ", cmd)
