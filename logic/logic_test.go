79c1e9e7ecf0df90d4bd04419663b785e231e96a
Harman Gakhal <harmangakhal@alumni.ubc.ca>
2019-05-03 13:24:52 -0700

    Added more unit tests for wallet logic

--- logic/logic_test.go
+++ logic/logic_test.go
@@ -53,13 +53,25 @@ func TestCreateWallet(t *testing.T) {
 }
 
 func TestCreateWalletWithPassphrase(t *testing.T) {
-	wallet, err := CreateWallet(GetTestWalletPath(), "test")
+	wallet, err := CreateWalletWithpassphrase("test", GetTestWalletPath())
 	assert.Nil(t, err)
 	pubKeyHash, ok := wallet.Addresses[0].GetPubKeyHash()
 	assert.Equal(t, true, ok)
 	walletPubKeyHash, err := core.NewUserPubKeyHash(wallet.Key.PublicKey)
 	assert.Nil(t, err)
 	assert.Equal(t, pubKeyHash, []byte(walletPubKeyHash))
+}
+
+func TestCreateWalletWithPassphraseMismatch(t *testing.T) {
+	cleanUpDatabase()
+
+	_, err := CreateWalletWithpassphrase("test", GetTestWalletPath())
+	assert.Nil(t, err)
+
+	_, err = CreateWalletWithpassphrase("wrong_password", GetTestWalletPath())
+	assert.Error(t, err)
+
+	cleanUpDatabase()
 }
 
 func TestCreateBlockchain(t *testing.T) {
@@ -192,11 +204,11 @@ func TestIsWalletEmptyWallet(t *testing.T) {
 	wallet1, err := CreateWallet(GetTestWalletPath(), "test")
 	assert.NotEmpty(t, wallet1)
 	assert.Nil(t, err)
-	empty, err := IsTestWalletEmpty()
+	empty, err := IsWalletEmpty(GetTestWalletPath())
 	assert.Nil(t, err)
 	assert.Equal(t, false, empty)
 	cleanUpDatabase()
-	empty, err = IsTestWalletEmpty()
+	empty, err = IsWalletEmpty(GetTestWalletPath())
 	assert.Nil(t, err)
 	assert.Equal(t, true, empty)
 
@@ -219,6 +231,55 @@ func TestDeleteInvalidWallet(t *testing.T) {
 	assert.ElementsMatch(t, list, addressList)
 
 	//teardown :clean up database amd files
+	cleanUpDatabase()
+}
+
+func TestIsWalletLocked(t *testing.T) {
+	cleanUpDatabase()
+
+	_, err := CreateWallet(GetTestWalletPath(), "test")
+	assert.Nil(t, err)
+
+	status, err := IsWalletLocked(GetTestWalletPath())
+	assert.Nil(t, err)
+	assert.True(t, status)
+
+	cleanUpDatabase()
+}
+
+func TestNilSetLockWallet(t *testing.T) {
+	assert.Nil(t, SetLockWallet(GetTestWalletPath()))
+}
+
+func TestSetLockWallet(t *testing.T) {
+	cleanUpDatabase()
+
+	_, err := CreateWallet(GetTestWalletPath(), "test")
+	assert.Nil(t, err)
+
+	assert.Nil(t, SetLockWallet(GetTestWalletPath()))
+	status, err := IsWalletLocked(GetTestWalletPath())
+	assert.Nil(t, err)
+	assert.True(t, status)
+
+	cleanUpDatabase()
+
+	status, err = IsWalletLocked(GetTestWalletPath())
+	assert.Nil(t, err)
+	assert.False(t, status)
+}
+
+func TestSetUnLockWallet(t *testing.T) {
+	cleanUpDatabase()
+
+	_, err := CreateWallet(GetTestWalletPath(), "test")
+	assert.Nil(t, err)
+
+	assert.Nil(t, SetUnLockWallet(GetTestWalletPath()))
+	status, err := IsWalletLocked(GetTestWalletPath())
+	assert.Nil(t, err)
+	assert.False(t, status)
+
 	cleanUpDatabase()
 }
 
