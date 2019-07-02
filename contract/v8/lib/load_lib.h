0504b62d50ec8ac48e8960fcc0430a909f228904
Antonio <antonio0526@hotmail.com>
2019-07-02 11:23:02 -0700

    Refactor js library loading

--- contract/v8/lib/load_lib.h
+++ contract/v8/lib/load_lib.h
@@ -6,3 +6,4 @@ void LoadLibraries(Isolate *isolate, Local<Context> &context);
 void LoadBlockchainLibrary(Isolate *isolate, Local<Context> &context);
 void LoadStorageLibrary(Isolate *isolate, Local<Context> &context);
 void LoadSenderLibrary(Isolate *isolate, Local<Context> &context);
+void LoadLibrary(Isolate *isolate, Local<Context> &context, const char *filepath, const char *filename);
