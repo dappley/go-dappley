adfd08556efffa4ab40b88dc29575a2eb1d81dd2
chaoswlz <wulize1994@gmail.com>
2019-07-24 14:47:52 -0700

    add logic file

---
+++ logic/account_logic/address_logic.go
@@ -0,0 +1,13 @@
+package logic
+
+import "github.com/dappley/go-dappley/client"
+
+//isContract checks if an address is a Contract address
+func IsContract(a client.Address) (bool, error) {
+	pubKeyHash, ok := client.GeneratePubKeyHashByAddress(a)
+	if !ok {
+		return false, client.ErrInvalidAddress
+	}
+	pkh := client.PubKeyHash(pubKeyHash)
+	return pkh.IsContract()
+}
---
+++ logic/account_logic/address_logic_test.go
@@ -0,0 +1,50 @@
+package logic
+
+import (
+	"testing"
+
+	"github.com/dappley/go-dappley/client"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddress_IsContract(t *testing.T) {
+	tests := []struct {
+		name        string
+		address     client.Address
+		expectedRes bool
+		expectedErr error
+	}{
+		{
+			name:        "ContractAddress",
+			address:     client.NewAddress("cfSr89kUCpKxxaH8qgqYcnp7BqbMyND9Po"),
+			expectedRes: true,
+			expectedErr: nil,
+		},
+		{
+			name:        "UserAddress",
+			address:     client.NewAddress("dXnq2R6SzRNUt7ZANAqyZc2P9ziF6vYekB"),
+			expectedRes: false,
+			expectedErr: nil,
+		},
+		{
+			name:        "InvalidAddress",
+			address:     client.NewAddress("1Xnq2R6SzRNUt7ZANAqyZc2P9ziF6vYekB"),
+			expectedRes: false,
+			expectedErr: client.ErrInvalidAddress,
+		},
+		{
+			name:        "EmptyAddress",
+			address:     client.NewAddress(""),
+			expectedRes: false,
+			expectedErr: client.ErrInvalidAddress,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := IsContract(tt.address)
+			assert.Equal(t, tt.expectedRes, res)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
