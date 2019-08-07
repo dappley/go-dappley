cc52470dc146ffaab4470006dd637085b3e32bdc
Antonio <antonio0526@hotmail.com>
2019-08-07 10:30:17 -0700

    Modify blockchain manager test to use the decoupled block model

--- core/blockchain_manager_test.go
+++ core/blockchain_manager_test.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"github.com/dappley/go-dappley/core/block"
+	"github.com/dappley/go-dappley/logic/block"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -14,12 +16,12 @@ func TestBlockChainManager_NumForks(t *testing.T) {
 	bc := CreateBlockchain(account.NewAddress(""), storage.NewRamStorage(), nil, NewTransactionPool(nil, 100), nil, 100)
 	blk, err := bc.GetTailBlock()
 	require.Nil(t, err)
-	b1 := &Block{header: &BlockHeader{height: 1, prevHash: blk.GetHash(), nonce: 1}}
-	b1.header.hash = b1.CalculateHash()
-	b3 := &Block{header: &BlockHeader{height: 2, prevHash: b1.GetHash(), nonce: 3}}
-	b3.header.hash = b3.CalculateHash()
-	b6 := &Block{header: &BlockHeader{height: 3, prevHash: b3.GetHash(), nonce: 6}}
-	b6.header.hash = b6.CalculateHash()
+
+	b1 := block.NewBlockWithRawInfo(nil, blk.GetHash(), 1, 0, 1, nil)
+	b3 := block.NewBlockWithRawInfo(nil, b1.GetHash(), 3, 0, 2, nil)
+	b3.SetHash(lblock.CalculateHash(b3))
+	b6 := block.NewBlockWithRawInfo(nil, b3.GetHash(), 6, 0, 3, nil)
+	b6.SetHash(lblock.CalculateHash(b6))
 
 	err = bc.AddBlockContextToTail(&BlockContext{Block: b1, UtxoIndex: NewUTXOIndex(nil), State: NewScState()})
 	require.Nil(t, err)
@@ -29,14 +31,17 @@ func TestBlockChainManager_NumForks(t *testing.T) {
 	require.Nil(t, err)
 
 	// create first fork of height 3
-	b2 := &Block{header: &BlockHeader{height: 2, prevHash: b1.GetHash(), nonce: 2}}
-	b2.header.hash = b2.CalculateHash()
-	b4 := &Block{header: &BlockHeader{height: 3, prevHash: b2.GetHash(), nonce: 4}}
-	b4.header.hash = b4.CalculateHash()
-	b5 := &Block{header: &BlockHeader{height: 3, prevHash: b2.GetHash(), nonce: 5}}
-	b5.header.hash = b5.CalculateHash()
-	b7 := &Block{header: &BlockHeader{height: 4, prevHash: b4.GetHash(), nonce: 7}}
-	b7.header.hash = b7.CalculateHash()
+	b2 := block.NewBlockWithRawInfo(nil, b1.GetHash(), 2, 0, 2, nil)
+	b2.SetHash(lblock.CalculateHash(b2))
+
+	b4 := block.NewBlockWithRawInfo(nil, b2.GetHash(), 4, 0, 3, nil)
+	b4.SetHash(lblock.CalculateHash(b4))
+
+	b5 := block.NewBlockWithRawInfo(nil, b2.GetHash(), 5, 0, 3, nil)
+	b5.SetHash(lblock.CalculateHash(b5))
+
+	b7 := block.NewBlockWithRawInfo(nil, b4.GetHash(), 7, 0, 4, nil)
+	b7.SetHash(lblock.CalculateHash(b7))
 
 	/*
 		              b1
@@ -59,7 +64,7 @@ func TestBlockChainManager_NumForks(t *testing.T) {
 	require.Equal(t, 1, testGetNumForkHeads(bp))
 
 	// adding block that is not connected to BlockChain should be ignored
-	b8 := &Block{header: &BlockHeader{height: 4, prevHash: []byte{9}, nonce: 8}}
+	b8 := block.NewBlockWithRawInfo(nil, []byte{9}, 8, 0, 4, nil)
 	bp.CacheBlock(b8, 0)
 	require.Equal(t, 2, testGetNumForkHeads(bp))
 
@@ -68,8 +73,9 @@ func TestBlockChainManager_NumForks(t *testing.T) {
 	require.EqualValues(t, 3, longestFork)
 
 	// create a new fork off b6
-	b9 := &Block{header: &BlockHeader{height: 4, prevHash: b6.GetHash(), nonce: 9}}
-	b9.header.hash = b9.CalculateHash()
+	b9 := block.NewBlockWithRawInfo(nil, b6.GetHash(), 9, 0, 4, nil)
+	b9.SetHash(lblock.CalculateHash(b9))
+
 	bp.CacheBlock(b9, 0)
 	require.Equal(t, 3, testGetNumForkHeads(bp))
 
