c4a24f466f4b7ee679c56c3c016af938a32209be
Antonio <antonio0526@hotmail.com>
2019-08-12 14:32:29 -0700

    move block syncing commands from blockchain download manager to miner

--- logic/miner/interfaces.go
+++ logic/miner/interfaces.go
@@ -1,10 +1,26 @@
 package miner
 
-import "github.com/dappley/go-dappley/core/block"
+import (
+	"github.com/dappley/go-dappley/core/block"
+	"github.com/dappley/go-dappley/network/network_model"
+	"github.com/golang/protobuf/proto"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
 
 type Consensus interface {
 	GetBlockProduceNotifier() chan bool
 	Validate(*block.Block) bool
 	//Return the lib block and new block whether pass lib policy
 	CheckLibPolicy(*block.Block) (*block.Block, bool)
+}
+
+type NetService interface {
+	SendCommand(
+		commandName string,
+		message proto.Message,
+		destination peer.ID,
+		isBroadcast bool,
+		priority network_model.DappCmdPriority)
+	Listen(command string, handler network_model.CommandHandlerFunc)
+	Relay(dappCmd *network_model.DappCmd, destination peer.ID, priority network_model.DappCmdPriority)
 }
--- logic/miner/miner.go
+++ logic/miner/miner.go
@@ -1,29 +1,73 @@
 package miner
 
 import (
+	"github.com/dappley/go-dappley/common/hash"
 	"github.com/dappley/go-dappley/consensus"
+	"github.com/dappley/go-dappley/core/block"
+	"github.com/dappley/go-dappley/core/block/pb"
 	"github.com/dappley/go-dappley/core/blockchain"
+	"github.com/dappley/go-dappley/core/pb"
 	"github.com/dappley/go-dappley/logic/block_logic"
 	"github.com/dappley/go-dappley/logic/blockchain_logic"
+	"github.com/dappley/go-dappley/network/network_model"
+	"github.com/golang/protobuf/proto"
+	"github.com/libp2p/go-libp2p-core/peer"
 	logger "github.com/sirupsen/logrus"
 	"time"
 )
 
-const maxMintingTimeInMs = 2000
-const NanoSecsInMilliSec = 1000000
+const (
+	maxMintingTimeInMs = 2000
+	NanoSecsInMilliSec = 1000000
+
+	SendBlock    = "SendBlockByHash"
+	RequestBlock = "requestBlock"
+)
+
+var (
+	minerSubscribedTopics = []string{
+		SendBlock,
+		RequestBlock,
+	}
+)
 
 type Miner struct {
-	con      Consensus
-	producer *consensus.BlockProducer
-	bc       *blockchain_logic.Blockchain
+	con        Consensus
+	producer   *consensus.BlockProducer
+	bc         *blockchain_logic.Blockchain
+	netService NetService
 }
 
-func NewMiner(con Consensus, bc *blockchain_logic.Blockchain) *Miner {
-	return &Miner{
-		con:      con,
-		producer: consensus.NewBlockProducer(),
-		bc:       bc,
+func NewMiner(bc *blockchain_logic.Blockchain, con Consensus, netService NetService) *Miner {
+	miner := &Miner{
+		con:        con,
+		producer:   consensus.NewBlockProducer(),
+		bc:         bc,
+		netService: netService,
 	}
+	miner.ListenToNetService()
+	return miner
+}
+
+func (miner *Miner) ListenToNetService() {
+	if miner.netService == nil {
+		return
+	}
+
+	for _, command := range minerSubscribedTopics {
+		miner.netService.Listen(command, miner.GetCommandHandler(command))
+	}
+}
+
+func (miner *Miner) GetCommandHandler(commandName string) network_model.CommandHandlerFunc {
+
+	switch commandName {
+	case SendBlock:
+		return miner.SendBlockHandler
+	case RequestBlock:
+		return miner.RequestBlockHandler
+	}
+	return nil
 }
 
 func (miner *Miner) Start() {
@@ -80,4 +124,69 @@ func (miner *Miner) updateNewBlock(ctx *blockchain_logic.BlockContext) {
 		return
 	}
 	miner.BroadcastBlock(ctx.Block)
+}
+
+//RequestBlock sends a requestBlock command to its peer with pid through network module
+func (miner *Miner) RequestBlock(hash hash.Hash, pid peer.ID) {
+	request := &corepb.RequestBlock{Hash: hash}
+
+	miner.netService.SendCommand(RequestBlock, request, pid, network_model.Unicast, network_model.HighPriorityCommand)
+}
+
+//RequestBlockhandler handles when blockchain manager receives a requestBlock command from its peers
+func (miner *Miner) RequestBlockHandler(command *network_model.DappRcvdCmdContext) {
+	request := &corepb.RequestBlock{}
+
+	if err := proto.Unmarshal(command.GetData(), request); err != nil {
+		logger.WithFields(logger.Fields{
+			"name": command.GetCommandName(),
+		}).Info("BlockchainManager: parse data failed.")
+	}
+
+	block, err := miner.bc.GetBlockByHash(request.Hash)
+	if err != nil {
+		logger.WithError(err).Warn("BlockchainManager: failed to get the requested block.")
+		return
+	}
+
+	miner.SendBlockToPeer(block, command.GetSource())
+}
+
+//SendBlockToPeer unicasts a block to the peer with peer id "pid"
+func (miner *Miner) SendBlockToPeer(blk *block.Block, pid peer.ID) {
+
+	miner.SendBlock(blk, pid, network_model.Unicast)
+}
+
+//BroadcastBlock broadcasts a block to all peers
+func (miner *Miner) BroadcastBlock(blk *block.Block) {
+	var broadcastPid peer.ID
+	miner.SendBlock(blk, broadcastPid, network_model.Broadcast)
+}
+
+//SendBlock sends a SendBlock command to its peer with pid by finding the block from its database
+func (miner *Miner) SendBlock(blk *block.Block, pid peer.ID, isBroadcast bool) {
+
+	miner.netService.SendCommand(SendBlock, blk.ToProto(), pid, isBroadcast, network_model.HighPriorityCommand)
+}
+
+//SendBlockHandler handles when blockchain manager receives a sendBlock command from its peers
+func (miner *Miner) SendBlockHandler(command *network_model.DappRcvdCmdContext) {
+	pb := &blockpb.Block{}
+
+	//unmarshal byte to proto
+	if err := proto.Unmarshal(command.GetData(), pb); err != nil {
+		logger.WithError(err).Warn("BlockchainManager: parse data failed.")
+		return
+	}
+
+	blk := &block.Block{}
+	blk.FromProto(pb)
+	miner.Push(blk, command.GetSource())
+
+	if command.IsBroadcast() {
+		//relay the original command
+		var broadcastPid peer.ID
+		miner.netService.Relay(command.GetCommand(), broadcastPid, network_model.HighPriorityCommand)
+	}
 }
