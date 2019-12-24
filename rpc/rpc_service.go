54bb7b8b412674add9c345ec8e30aa96d99b92b1
fly <fly1992@163.com>
2019-12-24 11:18:03 +0800

    add log to stat client info

--- rpc/rpc_service.go
+++ rpc/rpc_service.go
@@ -20,6 +20,7 @@ package rpc
 import (
 	"context"
 	"github.com/dappley/go-dappley/consensus"
+	"google.golang.org/grpc/peer"
 	"strconv"
 	"strings"
 	"time"
@@ -232,7 +233,8 @@ func (rpcService *RpcService) RpcGetBlockByHeight(ctx context.Context, in *rpcpb
 
 // RpcSendTransaction Send transaction to blockchain created by account account
 func (rpcService *RpcService) RpcSendTransaction(ctx context.Context, in *rpcpb.SendTransactionRequest) (*rpcpb.SendTransactionResponse, error) {
-
+	peer, _ := peer.FromContext(ctx)
+	logger.WithField("ip", peer.Addr.String()).WithField("transaction ID",string(in.Transaction.Id)).Info("receive transaction info")
 	tx := &transaction.Transaction{nil, nil, nil, common.NewAmount(0), common.NewAmount(0), common.NewAmount(0), time.Now().UnixNano() / 1e6}
 
 	tx.FromProto(in.GetTransaction())
