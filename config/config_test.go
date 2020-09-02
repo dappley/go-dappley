8755049f7fb4ceb6b581d455e41bd46bc3dfce15
xiaoyoulei <xyl1932432873@163.com>
2020-09-02 17:28:50 +0800

    Fix pb testCase bug

--- config/config_test.go
+++ config/config_test.go
@@ -76,7 +76,6 @@ func TestLoadConfig(t *testing.T) {
 			expected: &configpb.Config{},
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			filename := tt.name + "_config.conf"
@@ -84,6 +83,7 @@ func TestLoadConfig(t *testing.T) {
 			defer os.Remove(filename)
 			config := &configpb.Config{}
 			LoadConfig(filename, config)
+			//fmt.Println("config.ConsensusConfig.tt.expected:",tt.expected)
 			assert.Equal(t, tt.expected, config)
 		})
 	}
