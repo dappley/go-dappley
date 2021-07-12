545c6a35e58aad6cb8d3c86d769929a7c4c176df
chaoswlz <wulize1994@gmail.com>
2021-07-12 15:28:26 -0700

    modify the code

--- logic/lblockchain/blockchain_manager.go
+++ logic/lblockchain/blockchain_manager.go
@@ -48,7 +48,7 @@ const (
 type Dynasty struct {
 	Height   uint64 `json:"height"`
 	Producer string `json:"addresses"`
-	Type     int    `json:"type"`
+	Kind     int    `json:"kind"`
 }
 
 var (
@@ -94,8 +94,8 @@ func (bm *BlockchainManager) SetNewDynastyByString(info, original string) {
 	if err != nil {
 		logger.Warn(err.Error())
 	}
-	bm.consensus.AddReplacement(original, dynasty.Producer, dynasty.Height, dynasty.Type)
-	logger.Info("change", dynasty.Type)
+	bm.consensus.AddReplacement(original, dynasty.Producer, dynasty.Height, dynasty.Kind)
+	logger.Info("change", dynasty.Kind)
 }
 
 func (bm *BlockchainManager) CheckDynast(height uint64) {
