942bb11502362a02e4c2b5ca3b9f221eb5dbd82f
joeymeffen <jmeffen@omnisolu.com>
2021-09-07 16:21:04 -0700

    allow use of did to find document

--- dapp/cli/update_did.go
+++ dapp/cli/update_did.go
@@ -5,10 +5,38 @@ import (
 	"fmt"
 
 	"github.com/dappley/go-dappley/core/account"
+	"github.com/dappley/go-dappley/logic"
+	"github.com/dappley/go-dappley/wallet"
 )
 
 func updateDIDCommandHandler(ctx context.Context, a interface{}, flags cmdFlags) {
+	dm, err := logic.GetDIDManager(wallet.GetDIDFilePath())
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		return
+	}
 	filepath := *(flags[flagFilePath].(*string))
+	did := *(flags[flagDID].(*string))
+	if did == "" && filepath == "" {
+		fmt.Println("Please provide either a file path or a DID.")
+		return
+	} else if did != "" && filepath != "" {
+		fmt.Println("Only provide one of the file path or the DID.")
+		return
+	}
+	if filepath == "" {
+		for _, didSet := range dm.DIDSets {
+			if didSet.DID == did {
+				filepath = didSet.FileName
+				fmt.Println("Found did document: ", filepath)
+				break
+			}
+		}
+	}
+	if filepath == "" {
+		fmt.Println("Could not find did.")
+		return
+	}
 
 	doc, err := account.ReadDocFile(filepath)
 	if err != nil {
